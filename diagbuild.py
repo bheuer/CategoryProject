f291ec6b3eab6a27404511720feb712030c1b6a8
Tibor Backhausz <tibor.backhausz@gmail.com>
2015-11-26 14:18:39 +0000

    Read and write Diagrams to/from LaTeX (tikzcd)

--- diagbuild.py
+++ diagbuild.py
@@ -37,15 +37,16 @@ def parseCell(cell):
         cell=cell[:match.start()] + cell[match.end():]
         direction=countDir(match.group('target'))
         style=match.group('style')
-        arrlist.append((direction[0],direction[1],match.group('name'),style))
+        captionstyle=match.group('whatever')
+        arrlist.append((direction[0],direction[1],match.group('name'),style,captionstyle))
     while(ar_re.search(cell)):
         match=ar_re.search(cell)
         cell=cell[:match.start()] + cell[match.end():]
         direction=countDir(match.group('target'))
         style=match.group('style')
-        arrlist.append((direction[0],direction[1],match.group('name'),style))
+        captionstyle=match.group('whatever')
+        arrlist.append((direction[0],direction[1],match.group('name'),style,captionstyle))
     return (cell.strip(),arrlist)
-        
         
 def processCellMatrix(n,k,cells):
     '''expects rectangular n*k array of LaTeX as list of lists, builds a diagram and constructs it'''
@@ -71,9 +72,10 @@ def processCellMatrix(n,k,cells):
             assert objs.has_key((i,j))
             for ar in celldata[1]:
                 if not mrphs.has_key((i,j,i+ar[0],j+ar[1])):
-                    mrphs[(i,j,i+ar[0],j+ar[1])]=[(ar[2],ar[3])]
+                    mrphs[(i,j,i+ar[0],j+ar[1])]=[(ar[2],ar[3],ar[4])]
                 else:
-                    mrphs[(i,j,i+ar[0],j+ar[1])].append((ar[2],ar[3]))
+                    mrphs[(i,j,i+ar[0],j+ar[1])].append((ar[2],ar[3],ar[4]))
+            objs[(i,j)].gridpos=(i,j)
     #now that all objects exist we can construct morphisms
     for (i1,j1,i2,j2) in mrphs:
         if mrphs.has_key((i1,j1,i2,j2)):
@@ -85,13 +87,12 @@ def processCellMatrix(n,k,cells):
         if mrphs.has_key((i1,j1,i2,j2)):
             for morph in mrphs[(i1,j1,i2,j2)]:
                 if morph[0]==None:               #unnamed morphism
-                    Morphism(objs[(i1,j1)],objs[(i2,j2)])
+                    newmorph=Morphism(objs[(i1,j1)],objs[(i2,j2)])
                 else:
                     try:
-                        Morphism(objs[(i1,j1)],objs[(i2,j2)],morph[0].strip())
+                        newmorph=Morphism(objs[(i1,j1)],objs[(i2,j2)],morph[0].strip())
                     except ValueError:              #name already taken?
                         newname=morph[0].strip()+r"'"
-                        print newname
                         for safety_counter in xrange(1000):#this is to avoid an infinite loop
                             try:                           #if ValueError is caused by something other than name collision
                                newmorph=Morphism(objs[(i1,j1)],objs[(i2,j2)],newname)
@@ -99,7 +100,9 @@ def processCellMatrix(n,k,cells):
                             except ValueError:
                                 newname=newname+r"'"
                         raise ValueError
-                            
+                newmorph.style=morph[1]
+                newmorph.latex=morph[0]
+                newmorph.captionstyle=morph[2]
     return D
                         
 def diagBuild(diag):
@@ -109,9 +112,71 @@ def diagBuild(diag):
     brd=breakMatrix(diag)
     return processCellMatrix(brd[0],brd[1],brd[2])
 
+def Grid(D):
+    '''returns (n,k,grid) where grid is an n*k grid represented by a dict where (i,j) |--> object at (i,j)th position in grid'''
+    grid=dict()
+    maxi=0
+    maxj=0
+    for obj in D.Objects:
+        maxi=max(obj.gridpos[0],maxi)
+        maxj=max(obj.gridpos[1],maxj)
+        grid[obj.gridpos]=obj
+    return (maxi,maxj,grid)
+        
+def direction(morph):
+    '''returns the vector corresponding to the morphism'''
+    right=morph.target.gridpos[1]-morph.source.gridpos[1]
+    down=morph.target.gridpos[0]-morph.source.gridpos[0]
+    if(right>0):
+        if(down>0):
+            return "r"*right+"d"*down
+        else:
+            return "r"*right+"u"*(-down)
+    else:
+        if(down>0):
+            return "l"*(-right)+"d"*down
+        else:
+            return "l"*(-right)+"u"*(-down)
+            
+def latexDiag(D):
+        (maxi,maxj,grid)=Grid(D)
+        print grid
+        mrphs=dict()
+        for morph in D.MorphismList:
+            i=morph.source.gridpos[0]
+            j=morph.source.gridpos[1]
+
+            try:
+                mrphs[(i,j)].append(morph)
+            except:
+                mrphs[(i,j)]=[morph]
+        out=""
+        print (maxi,maxj)
+        for i in xrange(maxi+1):
+            for j in xrange(maxj+1):
+                print (i,j)
+                if grid.has_key((i,j)):
+                    out=out+grid[(i,j)].name + " "
+                    if mrphs.has_key((i,j)):
+                        for morph in mrphs[(i,j)]:
+                            out=out+" "+r"\arrow"
+                            if(morph.style):
+                                out=out+r"["+morph.style+r"]"
+                            out+=r"{"+direction(morph)+r"}"
+                            if(morph.captionstyle):
+                                out+="["+morph.captionstyle+r"]"
+                            if(morph.latex):
+                                out+=r"{"+morph.latex+"}"
+                out=out+r" &"
+            out=out+"\\\\ \n"
+        return out
+                
+        
+
 def test_diagBuild():
-    Dtest=r"&A\arrow[left hook]{ldd}[swap]{f}\arrow{rd}[description]{c}\arrow{rrd}[description]{d}\arrow{rrrd}[description]{e}\\&B\arrow{ld}\arrow{r}&C\arrow{r}&D\arrow{r}&E\\F"
+    Dtest=r"&A\arrow{ldd}[swap]{f}\arrow{rd}[description]{c}\arrow{rrd}[description]{d}\arrow{rrrd}[description]{e}\\&B\arrow{ld}\arrow{r}&C\arrow{r}&D\arrow{r}&E\\F"
     D=diagBuild(Dtest)
     assert D.Graph.nodes().__repr__()=="[A, C, B, E, D, F]"
     assert D.Graph.edges().__repr__()=="[(A, C), (A, E), (A, D), (A, F), (C, D), (B, C), (B, F), (D, E)]"
+    print latexDiag(D)
     return D
