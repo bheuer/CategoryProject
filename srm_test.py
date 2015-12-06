af699e219e88f6ffabc8fa34d468d76e730ad467
Tibor Backhausz <tibor.backhausz@gmail.com>
2015-12-06 19:24:04 +0000

    test for projectivity of coproduct of projectives; produces LaTeX document

--- srm_test.py
+++ srm_test.py
@@ -30,17 +30,54 @@ def test():
     return D
 
 
+def processDiagSequence(ds):
+    out=r"""\documentclass[a4paper]{article}
+           \usepackage[english]{babel}
+           \usepackage[utf8x]{inputenc}
+           \usepackage{amsmath}
+           \usepackage{graphicx}
+           \usepackage{tikz-cd}
+           \begin{document}"""
+    prev=""
+    for tex in ds:
+        if tex==prev:
+            continue
+        out+=r"$$\begin{tikzcd}"+"\n"
+        out+=tex
+        out+=r"\end{tikzcd}$$"+"\n"
+        prev=tex
+    out+=r"\end{document}"
+    return out
+
 def test2():        #fails because SimpleRuleMaster cannot do a composition yet
     D=Diagram()
     P1=Object(D,'P1')
     P1.latex='P_1'
     P2=Object(D,'P2')
     P1.latex='P_1'
+
+    id1=Identity(P1)
+    id2=Identity(P2)
+    print id1.name
+    
     P1xP2=Object(D,'P1xP2')
     P1xP2.latex="P_1 \oplus P_2"
-    pi1=Morphism(P1xP2,P1,'pi1')
-    pi2=Morphism(P1xP2,P2,'pi2')
-    ProductProperty(pi1,pi2)
+
+    idprod=Identity(P1xP2)
+    print idprod.name
+    
+#    pi1=Morphism(P1xP2,P1,'pi1')
+#    pi2=Morphism(P1xP2,P2,'pi2')
+    i1=Morphism(P1,P1xP2,'i1')
+    i2=Morphism(P2,P1xP2,'i2')
+#    ProductProperty(pi1,pi2)
+    CoProductProperty(i1,i2)
+
+    
+
+#    Commute(pi1*i1,id1)
+#    Commute(pi2*i2,id2)
+    
     B=Object(D,'B')
     C=Object(D,'C')
     pi=Morphism(B,C)
@@ -51,12 +88,27 @@ def test2():        #fails because SimpleRuleMaster cannot do a composition yet
     Projective(P1)
     Projective(P2)
 
-    SRM = SimpleRuleMaster(D,maxMorphisms=2)
-    for i in xrange(30):
-        SRM(verbose=True)
+    P1.gridpos=(0,0)
+    P2.gridpos=(0,2)
+    P1xP2.gridpos=(0,1)
+    B.gridpos=(1,1)
+    C.gridpos=(2,1)
+    P1.latex='P_1'
+    P2.latex='P_2'
+    P1xP2.latex='P_1 \oplus P_2'
+    B.latex='B'
+    C.latex='C'
+
+    SRM = SimpleRuleMaster(D,maxMorphisms=1)
+    diagSequence=[]
+    while(True):
+        result=SRM(numberOfExtensions=10,verbose=True,printlatex=True)
+        if (result==False):
+            break
+        diagSequence.append(result)
+    print processDiagSequence(diagSequence)
     return D
-    
-D=test()
-DisplayAll(D)
-DisplayAllMorphisms(D)
-print latexDiag(D)
+
+D=test2()
+#DisplayAll(D)
+
